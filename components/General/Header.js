2e135e3d7ec5a1d84edee951b574f01674e12f7a
Paul McDonald <pmcdonald1012@gmail.com>
2022-11-14 13:56:31 -0700

    added styling to header frame

--- components/General/Header.js
+++ components/General/Header.js
@@ -1,7 +1,14 @@
 import s from "../../styles/General/Header.module.css"
 const Header = () => {
   return (
-    <div className={s.container}>Header</div>
+    <>
+    <div className={s.semicircle}></div>
+    <div className={s.container}>
+     
+        <div className={s.subcontainer}>
+        </div> 
+    </div>
+    </>
   )
 }
 
