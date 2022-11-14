649f5b92282bdc6f58fc174d2e0f57de22d240f2
Paul McDonald <pmcdonald1012@gmail.com>
2022-11-14 16:48:38 -0700

    added logo, worked on header comp

--- components/General/Header.js
+++ components/General/Header.js
@@ -1,13 +1,14 @@
 import s from "../../styles/General/Header.module.css"
+import Logo from '../../public/Logo.png'
+import Image from "next/image"
 const Header = () => {
   return (
     <>
-    <div className={s.semicircle}></div>
-    <div className={s.container}>
-     
+        <div className={s.semicircle}>
+          <Image className={s.logo} src={Logo} alt='Logo' width={85} height={137}/>
+        </div>
         <div className={s.subcontainer}>
         </div> 
-    </div>
     </>
   )
 }
