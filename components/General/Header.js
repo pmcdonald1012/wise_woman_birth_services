c24cf3c00c4a666236529c57848f2f1d17c2f082
Paul McDonald <pmcdonald1012@gmail.com>
2022-11-16 15:18:57 -0700

    configured links on header and added some style changes with animations

--- components/General/Header.js
+++ components/General/Header.js
@@ -1,39 +1,85 @@
-import s from "../../styles/General/Header.module.css"
-import Logo from '../../public/Logo.png'
-import Image from "next/image"
-import { Beau_Rivage } from '@next/font/google'
-import { IoIosArrowDown } from 'react-icons/io'
-const inter = Beau_Rivage({ weight: '400'})
-const iconStyle = { color: "#933466", height: "20px" }
+import s from "../../styles/General/Header.module.css";
+import Logo from "../../public/Logo.png";
+import Image from "next/image";
+import { Beau_Rivage } from "@next/font/google";
+import { motion } from "framer-motion";
+import Link from "next/link";
+const inter = Beau_Rivage({ weight: "400" });
 const Header = () => {
   return (
-    <> 
-        <div className={s.semicircle}>
-          <Image className={s.logo} src={Logo} alt='Logo' width={85} height={137}/>
-          <div className={s.namecontainer}>
-           <h2 className={inter.className}>Wise Woman</h2>
-           <h3 className={inter.className}>Birth Services</h3>
-          </div>
+    <>
+      <div className={s.semicircle}>
+        <Image
+          className={s.logo}
+          src={Logo}
+          alt="Logo"
+          width={85}
+          height={137}
+        />
+        <div className={s.namecontainer}>
+          <h2 className={inter.className}>Wise Woman</h2>
+          <h3 className={inter.className}>Birth Services</h3>
         </div>
-        <div className={s.subcontainer}>
-          <h2 id={s.home_btn}>
-            <btn>Home</btn>
-          </h2>
-          <h2 id={s.about_btn}>
-            <btn>About</btn>
-          </h2>
-          <div id={s.spacer}></div>
-          <div id={s.spacer}></div>
-          <div id={s.spacer}></div>
-          <h2 id={s.services_btn}>
-            <btn>Services<IoIosArrowDown style={iconStyle}/></btn>
-            </h2>
-          <h2 id={s.contact_btn}>
-            <btn>Contact</btn>
-          </h2>
-        </div> 
+      </div>
+      <div className={s.subcontainer}>
+        <motion.h2
+          className={s.btn}
+          whileHover={{
+            color: "#933466",
+            scale: 1.2,
+            borderBottomColor: "#933466",
+          }}
+          id={s.home_btn}
+        >
+          <Link className={s.link} href="/">
+            Home
+          </Link>
+        </motion.h2>
+        <motion.h2
+          className={s.btn}
+          whileHover={{
+            color: "#933466",
+            scale: 1.2,
+            borderBottomColor: "#933466",
+          }}
+          id={s.about_btn}
+        >
+          <Link className={s.link} href="/about">
+            About
+          </Link>
+        </motion.h2>
+        <div id={s.spacer}></div>
+        <div id={s.spacer}></div>
+        <div id={s.spacer}></div>
+        <motion.h2
+          className={s.btn}
+          whileHover={{
+            color: "#933466",
+            scale: 1.2,
+            borderBottomColor: "#933466",
+          }}
+          id={s.services_btn}
+        >
+          <Link className={s.link} href="/services">
+            Services
+          </Link>
+        </motion.h2>
+        <motion.h2
+          className={s.btn}
+          whileHover={{
+            color: "#933466",
+            scale: 1.2,
+            borderBottomColor: "#933466",
+          }}
+          id={s.contact_btn}
+        >
+          <Link className={s.link} href="/contact">
+            Contact
+          </Link>
+        </motion.h2>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,7 @@
-import Header from '../components/General/Header'
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-      <Component {...pageProps} />
-  )
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
