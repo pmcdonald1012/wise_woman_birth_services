5a4db86bcf0bf1b7957697d7c33f9bfae3aabae2
Paul McDonald <pmcdonald1012@gmail.com>
2022-11-17 13:16:21 -0700

    finished services cards on home page, and added animation

--- components/Home/BirthServicesCard.js
+++ components/Home/BirthServicesCard.js
@@ -1,3 +1,28 @@
+import s from "../../styles/Home/Cards.module.css";
+import { useInView } from "framer-motion";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
 export const BirthServicesCard = () => {
-  return <div>BirthServicesCard</div>;
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+
+  return (
+    <Link id={s.link} href="/services">
+      <div
+        ref={ref}
+        className={s.container}
+        style={{
+          transform: isInView ? "none" : "translateX(-25rem)",
+          opacity: isInView ? 1 : 0,
+          transition: "1s",
+        }}
+      >
+        <motion.div whileHover={{ scale: 1.1 }}>
+          <div id={s.title}>Birth Services</div>
+          <div id={s.img}></div>
+        </motion.div>
+      </div>
+    </Link>
+  );
 };
--- components/Home/PPCCard.js
+++ components/Home/PPCCard.js
@@ -1,3 +1,27 @@
+import s from "../../styles/Home/Cards.module.css";
+import { useInView } from "framer-motion";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
 export const PPCCard = () => {
-  return <div>PPCCard</div>;
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+  return (
+    <Link id={s.link} href="/services">
+      <div
+        ref={ref}
+        className={s.container}
+        style={{
+          transform: isInView ? "none" : "translateX(25rem)",
+          opacity: isInView ? 1 : 0,
+          transition: "1s",
+        }}
+      >
+        <motion.div whileHover={{ scale: 1.1 }}>
+          <div id={s.title}>Birth Services</div>
+          <div id={s.img1}></div>
+        </motion.div>
+      </div>
+    </Link>
+  );
 };
