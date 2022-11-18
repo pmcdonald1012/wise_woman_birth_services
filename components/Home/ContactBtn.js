1038ac389a44862bcb0c03eecde826ebd6123c2c
Paul McDonald <pmcdonald1012@gmail.com>
2022-11-18 13:15:46 -0700

    finished home page without media querys

--- components/Home/ContactBtn.js
+++ components/Home/ContactBtn.js
@@ -1,7 +1,16 @@
+import s from "../../styles/Home/Home.module.css";
+import { motion, transform } from "framer-motion";
+import Link from "next/link";
+import { withRouter } from "next/router";
 export const ContactBtn = () => {
   return (
-    <div>
-      <btn>Contact Now</btn>
-    </div>
+    <Link className={s.ContactBtn2} href="/contact">
+      <motion.div
+        className={s.contactBtn}
+        whileHover={{ scale: 1.1, backgroundColor: "#933466", color: "white" }}
+      >
+        <btn>Contact Now!</btn>
+      </motion.div>
+    </Link>
   );
 };
