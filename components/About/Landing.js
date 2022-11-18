f4301929e6d1f4ace7aace6603117b31bd7ce500
Paul McDonald <pmcdonald1012@gmail.com>
2022-11-18 14:39:36 -0700

    added Landing on about page

---
+++ components/About/Landing.js
@@ -0,0 +1,14 @@
+import s from "../../styles/About/About.module.css";
+const Landing = () => {
+  return (
+    <div className={s.container}>
+      <div className={s.img1Cont}></div>
+      <div className={s.aboutCont}>
+        <h1>About Lisa</h1>
+      </div>
+      <div className={s.img2Cont}></div>
+    </div>
+  );
+};
+
+export default Landing;
