8498656369dc81ab3fba6a5de90ab35dda801877
Paul McDonald <pmcdonald1012@gmail.com>
2022-11-30 15:43:08 -0700

    finished contact page functionality, just need to clean up a couple things

--- components/Contact/ContactForm.js
+++ components/Contact/ContactForm.js
@@ -1,28 +1,59 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import s from "../../styles/Contact/ContactForm.module.css";
 
 const ContactForm = ({ setModalShow }) => {
+  const [hasValue, sethasValue] = useState("");
+  const [hasValue2, sethasValue2] = useState("");
+  const [hasValue3, sethasValue3] = useState("");
+  const [noValue, setNoValue] = useState(true);
+
   const form = useRef();
 
+  useEffect(() => {
+    if (hasValue && hasValue2 && hasValue3) {
+      setNoValue(false);
+    } else {
+      setNoValue(true);
+    }
+    console.log(noValue);
+  }, [hasValue, hasValue2, hasValue3]);
+
   const sendEmail = (e) => {
-    setModalShow(true);
     e.preventDefault();
-    // emailjs
-    //   .sendForm(
-    //     "service_x4bqfck",
-    //     "template_zjd6r81",
-    //     form.current,
-    //     "dRrL7dSzvHP1dGxg0"
-    //   )
-    //   .then(
-    //     (result) => {
-    //       console.log(result.text);
-    //     },
-    //     (error) => {
-    //       console.log(error.text);
-    //     }
-    //   );
+    if (!noValue) {
+      e.target.value = "";
+      emailjs
+        .sendForm(
+          "service_x4bqfck",
+          "template_zjd6r81",
+          form.current,
+          "dRrL7dSzvHP1dGxg0"
+        )
+        .then(
+          (result) => {
+            setModalShow(true);
+            sethasValue("");
+            sethasValue2("");
+            sethasValue3("");
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    } else {
+      setNoValue(true);
+    }
+  };
+
+  const handleChange = (event) => {
+    sethasValue(event.target.value);
+  };
+  const handleChange2 = (event) => {
+    sethasValue2(event.target.value);
+  };
+  const handleChange3 = (event) => {
+    sethasValue3(event.target.value);
   };
 
   return (
@@ -33,6 +64,8 @@ const ContactForm = ({ setModalShow }) => {
           type="text"
           name="from_name"
           placeholder="Name"
+          onChange={handleChange}
+          value={hasValue}
         />
       </label>
       <label className={s.customField}>
@@ -41,6 +74,8 @@ const ContactForm = ({ setModalShow }) => {
           type="email"
           name="user_email"
           placeholder="Your Email"
+          onChange={handleChange2}
+          value={hasValue2}
         />
       </label>
       <label className={s.customField}>
@@ -48,8 +83,11 @@ const ContactForm = ({ setModalShow }) => {
           className={s.messInput}
           name="message"
           placeholder="Message"
+          onChange={handleChange3}
+          value={hasValue3}
         />
       </label>
+      <div>{noValue ? "Please fill out all information" : null} </div>
       <input className={s.submitBtn} type="submit" value="Send"></input>
     </form>
   );
