8281a1e1dbc60e9243cbc35b5f0981585f73a9e4
Paul McDonald <pmcdonald1012@gmail.com>
2023-01-11 12:24:43 -0700

    fixed up the about page and added some new styling

--- components/About/Bio.js
+++ components/About/Bio.js
@@ -3,45 +3,49 @@ const Bio = () => {
   return (
     <div className={s.container}>
       <div className={s.subContainer1}>
-        <div className={s.title}>Mother Of Nine</div>
-        <div className={s.bioPara}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci a
-            scelerisque purus semper eget duis. Aliquam nulla facilisi cras
-            fermentum odio eu feugiat pretium nibh. Amet luctus venenatis lectus
-            magna fringilla urna porttitor rhoncus dolor. Sed turpis tincidunt
-            id aliquet risus feugiat in ante metus. Amet commodo nulla facilisi
-            nullam vehicula. Facilisi cras fermentum odio eu feugiat pretium
-            nibh ipsum consequat. Vel fringilla est ullamcorper eget nulla
-            facilisi etiam dignissim diam. Sem viverra aliquet eget sit amet
-            tellus cras adipiscing. Quis viverra nibh cras pulvinar. Orci a
-            scelerisque purus semper eget duis at tellus. Maecenas accumsan
-            lacus vel facilisis volutpat est.
-          </p>
+        <div className={s.subsubCont}>
+          <div className={s.title}>Mother Of Nine</div>
+          <div className={s.bioPara}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci a
+              scelerisque purus semper eget duis. Aliquam nulla facilisi cras
+              fermentum odio eu feugiat pretium nibh. Amet luctus venenatis
+              lectus magna fringilla urna porttitor rhoncus dolor. Sed turpis
+              tincidunt id aliquet risus feugiat in ante metus. Amet commodo
+              nulla facilisi nullam vehicula. Facilisi cras fermentum odio eu
+              feugiat pretium nibh ipsum consequat. Vel fringilla est
+              ullamcorper eget nulla facilisi etiam dignissim diam. Sem viverra
+              aliquet eget sit amet tellus cras adipiscing. Quis viverra nibh
+              cras pulvinar. Orci a scelerisque purus semper eget duis at
+              tellus. Maecenas accumsan lacus vel facilisis volutpat est.
+            </p>
+          </div>
         </div>
       </div>
       <div className={s.title2}>Promoting Healthy Births</div>
       <div className={s.subContainer2}>
-        <div className={s.quoteContainer}>
-          <div className={s.quote}>
-            {
-              "“ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. “"
-            }
+        <div className={s.subsubCont2}>
+          <div className={s.quoteContainer}>
+            <div className={s.quote}>
+              {
+                "“ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. “"
+              }
+            </div>
           </div>
+          <p>
+            Ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia.
+            Arcu risus quis varius quam quisque id diam. Amet luctus venenatis
+            lectus magna fringilla urna porttitor rhoncus dolor. Sed turpis
+            tincidunt id aliquet risus feugiat in ante metus. Amet commodo nulla
+            facilisi nullam vehicula. Facilisi cras fermentum odio eu feugiat
+            pretium nibh ipsum consequat. Vel fringilla est ullamcorper eget
+            nulla facilisi etiam dignissim diam. Sem viverra aliquet eget sit
+            amet tellus cras adipiscing. Quis viverra nibh cras pulvinar. Orci a
+            scelerisque purus semper eget duis at tellus. Maecenas accumsan
+            lacus vel facilisis volutpat est.
+          </p>
         </div>
-        <p>
-          Ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia. Arcu
-          risus quis varius quam quisque id diam. Amet luctus venenatis lectus
-          magna fringilla urna porttitor rhoncus dolor. Sed turpis tincidunt id
-          aliquet risus feugiat in ante metus. Amet commodo nulla facilisi
-          nullam vehicula. Facilisi cras fermentum odio eu feugiat pretium nibh
-          ipsum consequat. Vel fringilla est ullamcorper eget nulla facilisi
-          etiam dignissim diam. Sem viverra aliquet eget sit amet tellus cras
-          adipiscing. Quis viverra nibh cras pulvinar. Orci a scelerisque purus
-          semper eget duis at tellus. Maecenas accumsan lacus vel facilisis
-          volutpat est.
-        </p>
       </div>
       <p className={s.copyRight}>
         © 2023 Wise Woman Birth Services. All Rights Reserved.
--- components/About/Profile.js
+++ components/About/Profile.js
@@ -1,14 +1,23 @@
+import { useInView } from "framer-motion";
+import { useRef } from "react";
 import s from "../../styles/About/Profile.module.css";
 const Profile = () => {
+  const ref = useRef();
+  const isInView = useInView(ref);
   return (
-    <div className={s.greyline}>
-      <div className={s.container}>
-        <div className={s.img}></div>
-        <div className={s.title}>
-          <h1>{"Hi, I'm Lisa"}</h1>
-          <div>
-            <p>Sub-Title</p>
-          </div>
+    <div className={s.container} ref={ref}>
+      <div className={s.img}></div>
+      <div
+        className={s.title}
+        style={{
+          transform: isInView ? "none" : "translateX(-100vw)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 1s",
+        }}
+      >
+        <h1>{"Hi, I'm Lisa"}</h1>
+        <div>
+          <p>Sub-Title</p>
         </div>
       </div>
     </div>
