41bd691f9dbbe746e8d0e01da6b2d106985a3c24
Paul McDonald <pmcdonald1012@gmail.com>
2024-01-04 13:34:12 -0700

    added about text

--- components/About/Bio.js
+++ components/About/Bio.js
@@ -7,18 +7,21 @@ const Bio = () => {
           <div className={s.title}>Mother Of Nine</div>
           <div className={s.bioPara}>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci a
-              scelerisque purus semper eget duis. Aliquam nulla facilisi cras
-              fermentum odio eu feugiat pretium nibh. Amet luctus venenatis
-              lectus magna fringilla urna porttitor rhoncus dolor. Sed turpis
-              tincidunt id aliquet risus feugiat in ante metus. Amet commodo
-              nulla facilisi nullam vehicula. Facilisi cras fermentum odio eu
-              feugiat pretium nibh ipsum consequat. Vel fringilla est
-              ullamcorper eget nulla facilisi etiam dignissim diam. Sem viverra
-              aliquet eget sit amet tellus cras adipiscing. Quis viverra nibh
-              cras pulvinar. Orci a scelerisque purus semper eget duis at
-              tellus. Maecenas accumsan lacus vel facilisis volutpat est.
+              Hi, I’m Lisa McDonald. I am a DONA-certified birth doula and a
+              CAPPA trained postpartum doula. I am also the mother of nine
+              children and the grandmother of seven. Having given birth nine
+              times, seven unique hospital births and two home births, I wish I
+              had had a doula by my side for all of it. After raising and
+              homeschooling my kids for over 34 years, I decided to pursue my
+              passion-birth work. It has been a fulfilling job accompanying
+              families on their journey to a safe and desired birth experience
+              and then ensuring that they have the best possible start as a
+              family unit. I love to provide the tools to my clients so they can
+              have the birth experience they want. Every time I am present to
+              see a new blessing come into this world, I feel honored. And when
+              I work with a new family, helping them identify their baby’s needs
+              and giving them tools to meet those needs, I am thankful. I love
+              to impart the wisdom I learned and earned with these families.
             </p>
           </div>
         </div>
@@ -27,23 +30,15 @@ const Bio = () => {
       <div className={s.subContainer2}>
         <div className={s.subsubCont2}>
           <div className={s.quoteContainer}>
-            <div className={s.quote}>
-              {
-                "“ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. “"
-              }
-            </div>
+            <div className={s.quote}>{"“ Birthing done the right way “"}</div>
           </div>
           <p>
-            Ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia.
-            Arcu risus quis varius quam quisque id diam. Amet luctus venenatis
-            lectus magna fringilla urna porttitor rhoncus dolor. Sed turpis
-            tincidunt id aliquet risus feugiat in ante metus. Amet commodo nulla
-            facilisi nullam vehicula. Facilisi cras fermentum odio eu feugiat
-            pretium nibh ipsum consequat. Vel fringilla est ullamcorper eget
-            nulla facilisi etiam dignissim diam. Sem viverra aliquet eget sit
-            amet tellus cras adipiscing. Quis viverra nibh cras pulvinar. Orci a
-            scelerisque purus semper eget duis at tellus. Maecenas accumsan
-            lacus vel facilisis volutpat est.
+            I am trained in Spinning Babies, Gena Kirby’s Rebozo for Doulas, and
+            Belly Binding. I attended college and nursing school at the
+            University of Colorado. When I am not busy taking care of mothers
+            and their families I love to read and workout-weight lifting,
+            swimming, dancing, and pickleball. I also love to spend time with my
+            expanding family.
           </p>
         </div>
       </div>
