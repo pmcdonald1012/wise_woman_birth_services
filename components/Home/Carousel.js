efefe97374a2d51918d6c8bb5f54217a59584d25
Paul McDonald <pmcdonald1012@gmail.com>
2024-01-08 17:41:54 -0700

    added testimonial text

--- components/Home/Carousel.js
+++ components/Home/Carousel.js
@@ -17,12 +17,14 @@ const CarouselComp = () => {
             <div className={s.embla__slide}>
               <div className={s.slide_container}>
                 <p>
-                  Ornare aenean euismod elementum nisi quis. Quam quisque id
-                  diam vel quam elementum pulvinar etiam non. Libero id faucibus
-                  nisl tincidunt eget. tempus urna et pharetra pharetra massa
-                  massa.
+                  We had a fantastic experience with Lisa. she is one of a kind.
+                  Very laid back and so easy to get along with. Lisa went above
+                  and beyond to visit us at home, make sure we were prepared for
+                  our unmedicated hospital birth and to provide comfort and
+                  support where we needed it. She is amazing and I highly
+                  recommend her as a doula to any family.
                 </p>
-                <h3>Erica</h3>
+                <h3>L.K.</h3>
               </div>
             </div>
             <div className={s.embla__slide}>
