62c97d9772164355b4b9e5dda73b29a8416032e8
Paul McDonald <pmcdonald1012@gmail.com>
2024-01-08 20:36:29 -0700

    added final testimonials

--- components/Home/Carousel.js
+++ components/Home/Carousel.js
@@ -24,27 +24,35 @@ const CarouselComp = () => {
                   support where we needed it. She is amazing and I highly
                   recommend her as a doula to any family.
                 </p>
-                <h3>L.K.</h3>
+                <h3>~L.K.</h3>
               </div>
             </div>
             <div className={s.embla__slide}>
               <div className={s.slide_container}>
                 <p>
-                  Ornare elementum nisi quis. Quam quisque id diam vel quam
-                  elementum pulvinar etiam non. Libero id faucibus nisl
-                  tincidunt eget. Egestas sed.
+                  Once my husband and I decided that we wanted to consider
+                  hiring a doula we knew the right person for our family was
+                  Lisa. From the very beginning she put us at ease, educated us
+                  and made pregnancy then labor and delivery during a pandemic a
+                  little easier.
                 </p>
-                <h3>Marty</h3>
+                <h3>~C.W.</h3>
               </div>
             </div>
             <div className={s.embla__slide}>
               <div className={s.slide_container}>
                 <p>
-                  Quam quisque id diam vel quam elementum pulvinar etiam non.
-                  Libero id faucibus nisl tincidunt eget. Egestas sed tempus
-                  urna et pharetra pharetra massa massa.
+                  Lisa McDonald was an amazing doula during my last few weeks of
+                  pregnancy and especially during labor. Since this was my first
+                  child, I had almost no idea on how to approach the impending
+                  birth. I knew I wanted it to be a natural birth, but I also
+                  knew I was going to be delivering in a hospital. Lisa listened
+                  to my wants and concerns as an experienced mother and
+                  knowledgeable doula, giving me many resources on how to
+                  prepare myself for a natural labor and how to minimize time
+                  spent at the hospital.
                 </p>
-                <h3>Jason</h3>
+                <h3>~M.P.</h3>
               </div>
             </div>
           </div>
